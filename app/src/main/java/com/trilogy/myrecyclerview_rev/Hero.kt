0729df5a4dd87c022d3e361bda03b5f0d2ae1b54
Herdiyan Adam Putra <herdiyanadam256@gmail.com>
2023-09-24 12:58:40 +0700

    Initial Commit

---
+++ app/src/main/java/com/trilogy/myrecyclerview_rev/Hero.kt
@@ -0,0 +1,11 @@
+package com.trilogy.myrecyclerview_rev
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Hero(
+    val name: String,
+    val description: String,
+    val photo: Int
+): Parcelable
